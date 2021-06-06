139569fd7cfb2a57afc0a0ccd043fb302d000f26
Dawid Król <dawikrol@gmail.com>
2021-06-06 19:44:30 +0200

    Add Post model

--- blog_app/models.py
+++ blog_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+#from django.utils import timezone
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)  # default
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
