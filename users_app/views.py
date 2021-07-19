69a7285a298abdec7b6d97d8cfeccbefc3fc68b9
unknown <dawikrol@gmail.com>
2021-07-19 21:42:31 +0200

    Add login and logout system

--- users_app/views.py
+++ users_app/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegisterForm
-
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -12,8 +12,13 @@ def register(request):
 		if form.is_valid():
 			form.save()  #  save user in database
 			username = form.cleaned_data.get('username')
-			messages.success(request, f'Account created for {username}')
-			return redirect('blog_app-home')
+			messages.success(request, f'Your account has been created.')
+			return redirect('login')
 	else:
 		form = UserRegisterForm(request.POST)
 	return render(request, 'users_app/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+	return render(request, 'users_app/profile.html')
