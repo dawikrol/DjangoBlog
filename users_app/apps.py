8854b4b2a1316e3b65a66d37648f8e338edf216c
unknown <dawikrol@gmail.com>
2021-07-20 20:36:51 +0200

    Add user profile

--- users_app/apps.py
+++ users_app/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'users_app'
+
+    def ready(self):
+        import users_app.signals
