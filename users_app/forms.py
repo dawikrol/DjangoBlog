ea61beba0fc5ab4d43359b16a5aef9cbcdd1ebec
unknown <dawikrol@gmail.com>
2021-07-24 13:07:47 +0200

    Add profile updateing features

--- users_app/forms.py
+++ users_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +10,16 @@ class UserRegisterForm(UserCreationForm):
 	class Meta:  # this class give nested name space for configuration
 		model = User
 		fields = ['username', 'email', 'password', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+	email = forms.EmailField()
+
+	class Meta:  # this class give nested name space for configuration
+		model = User
+		fields = ['username', 'email',]
+
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['image']
