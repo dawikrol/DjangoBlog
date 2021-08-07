2bb7623a9322992bbabbf3c41ac0fc816f1eb414
unknown <dawikrol@gmail.com>
2021-08-07 19:22:25 +0200

    Add views as classes and templates: post_details, home

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import PostListView, PostDetailView
 
 urlpatterns = [
-    path('', views.home, name='blog_app-home'),
+    path('', PostListView.as_view(), name='blog_app-home'),
     path('about/', views.about, name='blog_app-about'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-details'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 def home(request):
@@ -8,6 +9,20 @@ def home(request):
 		'posts': Post.objects.all(),
 	}
 	return render(request, 'blog_app/home.html', context)
+
+
+class PostListView(ListView):
+	# class as view (home)
+	model = Post
+	template_name = 'blog_app/home.html'  # we need to change default name of view <app>/<model>_view_type>.html
+	context_object_name = 'posts'  # we need to change default context name
+	ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+	model = Post
+	template_name = 'blog_app/post_details.html'
+
 
 
 def about(request):
