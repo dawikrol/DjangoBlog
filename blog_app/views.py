0fdfdedc757faefa31053b1bb3967bf9787c14ec
unknown <dawikrol@gmail.com>
2021-08-15 23:31:10 +0200

    Add UserPostView

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import (ListView, DetailView, CreateView, UpdateView, DeleteView)
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -19,6 +20,19 @@ class PostListView(ListView):
 	context_object_name = 'posts'  # we need to change default context name
 	ordering = ['-date_posted']
 	paginate_by = 5
+
+
+class UserPostListView(ListView):
+	# class as view (home)
+	model = Post
+	template_name = 'blog_app/user_posts.html'
+	context_object_name = 'posts'
+	# ordering = ['-date_posted'] below we use another method
+	paginate_by = 5
+
+	def get_queryset(self):
+		user = get_object_or_404(User, username=self.kwargs.get('username'))
+		return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
